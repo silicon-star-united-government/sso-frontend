f08b4c21b0878e89faa5185074bc2d6c11f1bd42
3swordman <yyuxiaoran@qq.com>
2024-07-31 15:10:19 +0800

    add license details

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,12 @@ import banner from "vite-plugin-banner"
 const comment = `
 /**
  * The frontend part of SSUG SSO system
+ * 
+ * Copyright (C) 2024 SSUG
+ * This program is free software: you can redistribute it and/or modify it under the terms of the BSD 4-Clause License.
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the BSD 4-Clause License for more details.
+ * You should have received a copy of the BSD 4-Clause License along with this program. If not, see <https://directory.fsf.org/wiki/License:BSD-4-Clause>.
+ * 
  * @copyright SSUG
  * @author 3swordman
  * @license BSD-4-Clause
